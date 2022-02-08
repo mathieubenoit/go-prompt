78bb3660baf23b47a591e7bb3089c88bfcfd663c
mbenoit <mathieu.benoit@cern.ch>
2022-02-08 17:43:50 -0500

    change internal packages

--- document.go
+++ document.go
@@ -4,8 +4,8 @@ import (
 	"strings"
 	"unicode/utf8"
 
-	"github.com/c-bata/go-prompt/internal/bisect"
-	istrings "github.com/c-bata/go-prompt/internal/strings"
+	"github.com/mathieubenoit/go-prompt/internal/bisect"
+	istrings "github.com/mathieubenoit/go-prompt/internal/strings"
 	runewidth "github.com/mattn/go-runewidth"
 )
 
